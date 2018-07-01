85babb3cc1839f899bffa49feea07085112e7426
Nate Sage <davidsage@Davids-MacBook-Pro.local>
2018-06-30 20:44:25 -0700

    feat: created ui for NewTweet Component

---
+++ src/components/NewTweet.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+// Controlled Component
+class NewTweet extends Component {
+  state = {
+    text: '',
+
+  }
+  handleChange = (e) => {
+    const text = e.target.value;
+
+    this.setState(() => ({
+      text
+    }))
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { text } = this.state;
+
+    // todo: Add Tweet to the Store
+
+    console.log('New Tweet: ', text);
+
+    this.setState(() => ({
+      text: ''
+    }))
+  }
+
+  render() {
+    const { text } = this.state;
+
+    {/* todo: Redirect to the homeview if submitted*/}
+
+    const tweetLeft = 280 - text.length;
+
+    return (
+      <div>
+        <h3 className='center'>Compose new Tweet</h3>
+        <form className='new-tweet' onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder='Whats happening up in heeeeereee'
+            value={text}
+            onChange={this.handleChange}
+            className='textarea'
+            maxLength={280}
+          />
+          {tweetLeft <= 100 && (
+            <div className='tweet-length'>
+              {tweetLeft}
+            </div>
+          )}
+          <button
+            className='btn'
+            type='submit'
+            disabled={text === ''}>
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default NewTweet;
